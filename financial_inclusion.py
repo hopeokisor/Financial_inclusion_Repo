0aaa76de9d733f6c88bad61ce0ae4e0029662468
hopeokisor <135638405+hopeokisor@users.noreply.github.com>
2023-10-19 20:52:03 +0100

    Update financial_inclusion.py

--- financial_inclusion.py
+++ financial_inclusion.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn
 import pickle
 
 #----------------------LOAD MODEL--------------------
